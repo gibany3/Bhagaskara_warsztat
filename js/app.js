a1ce2e890601280d0ea6272e0b140b8275da753d
gibany3 <gibany3@wp.pl>
2016-05-31 09:06:57 +0200

    Wersja beta

--- js/app.js
+++ js/app.js
@@ -17,18 +17,70 @@ $(document).ready(function() {
 
 
     //progress Bar
-
     var ourSkills = $('.ourSkills');
 
+    var stickyMenu = $(".menuScreen");
+    var shortDescSection = $(".iconSection");
+    var stickyMenuTrigger = shortDescSection.offset().top - stickyMenu.outerHeight();
+
+
+    var wasAnimated = false;
     $(window).scroll(function(event) {
         var currPosition = (ourSkills.offset().top) + ourSkills.height() - ($(window).innerHeight() * 1);
-        if ($(window).scrollTop() > currPosition) {
+        if ($(window).scrollTop() > currPosition && !wasAnimated) {
             $('.ProgBar').addClass('animate');
-            $(window).off('scroll');
             $('.ProgBar').prev('p').find('span:nth-of-type(2)').counterUp({
                 delay: 10,
                 time: 700
             });
+
+            wasAnimated = true;
+        };
+
+        //Sticky menu
+        if ($(window).scrollTop() > stickyMenuTrigger) {
+            stickyMenu.addClass("sticky");
+        } else {
+            stickyMenu.removeClass("sticky");
+        };
+
+    });
+
+    //slider
+    var nextButton = $('.nextBtn');
+    var prevButton = $('.prevBtn');
+    var images = $('.slider').find('li').find('div');
+    var slider = $('.slider').find('ul');
+    var currentPicture = 0;
+    var widthImage = images.eq(currentPicture).width();
+
+
+    slider.width(widthImage * images.length);
+
+    nextButton.click(function() {
+        if (currentPicture < (images.length - 1)) {
+            currentPicture++;
+            slider.animate({
+                left: "-=" + widthImage
+            }, 1000);
+        };
+
+        // if (currentPicture < (members.length - 1) && (members.last().offset().left - (teamMembersSlider.offset().left + teamMembersSlider.width() - imageWidth)) != 0) {
+        //         currentPicture++;
+        //         membersList.animate({
+        //             left: "-=" + imageWidth
+        //         }, 1000);
+        //     };
+
+
+    });
+
+    prevButton.click(function() {
+        if (currentPicture > 0 && currentPicture < (images.length)) {
+            currentPicture--;
+            slider.animate({
+                left: "+=" + widthImage
+            }, 1000);
         };
     });
 
