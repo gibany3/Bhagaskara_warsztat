940c9c8a5f59037170416c8eff76b0c49e1048c9
gibany3 <gibany3@wp.pl>
2016-07-18 16:18:53 +0200

    Naprawa bledow

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
+// var gulp = require("gulp");
+// var sass = require("gulp-sass");
+//
+//
+//
+// gulp.task("scss", function() {
+//
+//     return gulp.scr("scss/*.scss")
+//         .pipe(sass({
+//             errLogToConsole: true,
+//             outputStyle: "expanded"
+//         }).on("error", sass.logError))
+//         .pipe(gulp.dest("css"));
+//
+// });
+//
+// gulp.task('watch', function() {
+//     gulp.watch('scss/*.scss', ['scss']);
+// });
+//
+// gulp.task('default', [watch],function() {});
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browser_sync = require('browser-sync');
